9a7edcc2f6a9102ce29e1f84f0a182db2d8fa70e
Andy Yuen <andyyuen287@gmail.com>
2022-09-26 10:59:17 -0700

    renamed frontend folder and added backend files

---
+++ sweetpress-backend/src/test/java/com/spring/sweetpress/SweetpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sweetpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
