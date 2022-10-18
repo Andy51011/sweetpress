21b9c842e827c6b20a331881a08b530e81315b57
dyuen510 <davidyuen0305@gmail.com>
2022-10-18 15:02:01 -0700

    mail to contact on catering section

--- sweetpress-frontend/src/components/catering/Catering.js
+++ sweetpress-frontend/src/components/catering/Catering.js
@@ -3,15 +3,22 @@ import Cater from '../../images/catering.png';
 import Lime from '../../images/lime.jpeg';
 import Dragonfruit from '../../images/dragonfruit.jpeg';
 import './Catering.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+
 
 const Catering = () => {
+    library.add(faEnvelope, faKey);
     return (
         <div className = 'catering_container'>
             <img src={Dragonfruit} width='750' height='400'/>
             <div className='catering_info'>
                 <h3>CATERING</h3>
                 <p>Contact us for catering options</p>
-                <button className='contact_button'>Contact</button>
+                <a href='mailto:sweetpress.sugarcane@gmail.com' className='mail__social'>
+                    <FontAwesomeIcon icon={faEnvelope} size='5x' color='darkslategray' />
+                </a>
             </div>    
         </div>
     )
